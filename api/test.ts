c93143318afeefc6af6afd8d1dac3b7cca6de026
Serhii Khrapin <khrapins@gmail.com>
2025-12-09 14:39:55 +0200

    Edit test API

--- api/test.ts
+++ api/test.ts
@@ -16,6 +16,7 @@ export default function handler(req, res) {
   const { cookies = "", headers = "", query = "", body = ""} = req
 
   res.setHeader('Access-Control-Allow-Origin', 'https://www.strava.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader("Content-Security-Policy", "connect-src 'https://www.strava.com/'")
 
   res.status(200).json({
