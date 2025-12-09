c9d42afe24b7cabbbc0ad5f007248940c30a01b3
Serhii Khrapin <khrapins@gmail.com>
2025-12-09 15:19:30 +0200

    Edit test API

--- api/test.ts
+++ api/test.ts
@@ -1,23 +1,8 @@
-// export default {
-//   async fetch(request: Request) {
-//     const url = new URL(request.url);
-//     const { cookies = "", headers = "", query = "", body = ""} = request
- 
-    // return Response.json({
-    //   cookies,
-    //   headers,
-    //   query,
-    //   body
-    // });
-//   },
-// };
-
 export default function handler(req, res) {
   const { cookies = "", headers = "", query = "", body = ""} = req
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.strava.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader("Content-Security-Policy", "connect-src https://www.strava.com/")
 
   res.status(200).json({
       cookies,
