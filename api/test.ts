2fa4428644d979f2e3dea0c141d663fe679cf7b6
Serhii Khrapin <khrapins@gmail.com>
2025-12-09 15:22:22 +0200

    Edit test API

--- api/test.ts
+++ api/test.ts
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   const { cookies = "", headers = "", query = "", body = ""} = req
 
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
   res.status(200).json({
