29071742454192e026544ca492d0af40e8a05ee1
Serhii Khrapin <khrapins@gmail.com>
2025-12-09 15:31:13 +0200

    Edit test API

--- api/test.ts
+++ api/test.ts
@@ -2,14 +2,12 @@ export default function handler(req, res) {
   const { cookies = "", headers = "", query = "", body = ""} = req
 
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   // Handle preflight request
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    return res.status(200).end();
   }
 
   res.status(200).json({
