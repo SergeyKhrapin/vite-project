28df2bdc8cf0a3dcade0c04725d4e9b082ec3aa4
Serhii Khrapin <khrapins@gmail.com>
2025-12-11 17:00:16 +0200

    Test CSRF

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect } from 'react'
+import { createContext } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
